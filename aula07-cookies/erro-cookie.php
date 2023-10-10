e58108913b6f85de23857ce545082ed10cfeb34d
Marcio Araya <marcio.araya@gmail.com>
2023-10-10 11:34:57 -0300

    cookies e sessão

---
+++ aula07-cookies/erro-cookie.php
@@ -0,0 +1,19 @@
+<?php
+
+
+echo '<html><body>';
+
+
+for($i = 0; $i <= 20; $i++) {
+    echo "<p>$i Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste Teste</p>";
+}
+   
+// a instrução setcookie deve ser enviada antes de dados que farão parte da página HTML
+setcookie("c_usuario", "Fulano", time()+3600);
+setcookie("c_idade", 20,  time()+3600);
+
+
+echo '<a href="le_cookie.php">Ler dados gravados nos cookies</a>';
+echo '</body></html>';
+
+?>
